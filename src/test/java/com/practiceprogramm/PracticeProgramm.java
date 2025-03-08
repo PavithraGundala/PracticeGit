e52cf47d8bbd1cc1f774e11632703849ceb1e3ea
PavithraGundala <pavithragundala036@gmail.com>
2025-03-08 12:58:52 +0530

    done

---
+++ src/test/java/com/practiceprogramm/PracticeProgramm.java
@@ -0,0 +1,16 @@
+package com.practiceprogramm;
+
+public class PracticeProgramm {
+	public void method1() {
+		System.out.println("method1");
+	}
+
+	public void method2() {
+		System.out.println("method2");
+	}
+
+	public void method3() {
+		System.out.println("method3");
+	}
+
+}
